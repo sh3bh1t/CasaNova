2bcc7841d6463bcb3aa6085dd8ac50cba369ac2a
Shobhit Srivastava <158823179+sh3bh1t@users.noreply.github.com>
2024-10-07 12:39:10 +0530

    Update init.js

--- init/init.js
+++ init/init.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 const initData= require(("./data.js"));
-const Listing=require("../../CasaNova/models/listing.js");
+const Listing=require("/models/listing.js");
 
 const Mongo_URL="mongodb://127.0.0.1:27017/CasaNova";
 
