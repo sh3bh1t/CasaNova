08d554941a1716852825327edbd208611cccb5e6
Shobhit Srivastava <158823179+sh3bh1t@users.noreply.github.com>
2024-10-07 12:46:19 +0530

    Update reviewRoute.js

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,11 +1,11 @@
  const express=require("express");
  const router = express.Router({mergeParams : true});
- const WrapAsync = require("/utils/WrapAsync.js");
- const ExpressError = require("/utils/ExpressError.js");
- const {listingSchemaValidation, reviewSchemaValidation} = require("/schemavalidation.js");
- const Review = require("/models/review.js");
- const Listing = require("/models/listing.js");
-const { addListingReview, deleteListingReview } = require("/controllers/review.js");
+ const WrapAsync = require("../utils/WrapAsync.js");
+ const ExpressError = require("../utils/ExpressError.js");
+ const {listingSchemaValidation, reviewSchemaValidation} = require("../schemavalidation.js");
+ const Review = require("../models/review.js");
+ const Listing = require("../models/listing.js");
+const { addListingReview, deleteListingReview } = require("../controllers/review.js");
  
  // REVIEWS PART
 
